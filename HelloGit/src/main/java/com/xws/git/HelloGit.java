7d410b224e4156e503897af5d03e77e5ff4d52c9
xws <xuweisheng7@qq.com>
2021-01-21 22:53:10 +0800

    更新1

--- HelloGit/src/main/java/com/xws/git/HelloGit.java
+++ HelloGit/src/main/java/com/xws/git/HelloGit.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("更新1");
     }
 }
