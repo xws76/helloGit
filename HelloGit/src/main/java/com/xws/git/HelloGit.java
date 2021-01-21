26a58e6f35f440c0f5f00281ad70c81c45a459bc
xws <xuweisheng7@qq.com>
2021-01-21 22:51:56 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/xws/git/HelloGit.java
@@ -0,0 +1,9 @@
+package com.xws.git;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
